7c767325ce096ec574067f957fd9ecb99d0c016f
Michael Braun <michael@web4brauns.de>
2022-03-01 21:44:01 +0100

    Add automatic zipping to webpack

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const ZipPlugin = require('zip-webpack-plugin');
+const { name, version } = require('../package.json');
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [
+        new ZipPlugin({
+            filename: `${name}_${version}.zip`,
+        }),
+    ]
 });
